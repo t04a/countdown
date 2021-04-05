79f5d79b6e7a96371f51fa2bde97a591c690ab10
Roman Roman <roman@roman.com>
2021-04-05 18:55:18 +0300

    перевел все в объект с методами

--- dist/main.js
+++ dist/main.js
@@ -1,106 +1,81 @@
+let pomodoro = {
+  time: 0,
+  initTimeMs: 0,
+  initTimer() {
+    this.secondsDom = document.querySelector('#countdown .seconds');
+    this.minutesDom = document.querySelector('#countdown .minutes');
+    this.time = this.initTimeMs = this.convTimeToMs();
+    console.log(this);
+    document.querySelector('.btn-timer').addEventListener('click', this.startTimer);
+    document.querySelector('.stop-music').addEventListener('click', this.pauseTimer);
+    document.querySelector('.btn.reset').addEventListener('click', this.resetTimer);
+  },
+  convTimeToMs() {
+    let secInMs, minInMs, timeInMs;
+    secInMs = this.secondsDom.innerHTML * 1000;
+    minInMs = this.minutesDom.innerHTML * 60000;
+    return timeInMs = secInMs + minInMs;
+  },
+  convMsToNormTime(timeMs) {
+    let minutes, seconds, normTime;
+    minutes = parseInt(timeMs / 60000);
+    seconds = (timeMs % 60000) / 1000;
+    normTime = {
+      minutes: minutes,
+      seconds: seconds,
+    }
+
+    return normTime;
+  },
+  startTimer() {
+    pomodoro.interval = setInterval(function() {
+      pomodoro.countdown();
+    }, 1000);
+  },
+  pauseTimer() {
+    clearInterval(pomodoro.interval);
+    console.log('pause timer');
+  },
+  resetTimer() {
+    clearInterval(pomodoro.interval);
+    pomodoro.updateDOM(pomodoro.time);
+    pomodoro.initTimeMs = pomodoro.time;
+    if(sessionStorage.getItem('time')) {
+      sessionStorage.removeItem('time');
+      console.log(sessionStorage.length);
+    }
+  },
+  countdown() {
+    console.log('timer 1st action');
+    
+    if (this.initTimeMs > 0) {
+      this.initTimeMs = this.initTimeMs - 1000;
+      sessionStorage.setItem('time', this.initTimeMs);
+      this.updateDOM(this.initTimeMs);
+    }
+    if(this.initTimeMs === 0) {
+      sessionStorage.removeItem('time');
+      clearInterval(this.interval);
+    }
+    
+    console.log(this);
+    console.log('timer last action');
+  },
+  updateDOM(time) {
+    console.log(time);
+    let normTime = this.convMsToNormTime(time);
+    console.log(normTime);
+    this.secondsDom.innerHTML = normTime.seconds;
+    this.minutesDom.innerHTML = normTime.minutes;
+  },
+
+};
+
 window.onload = function () {
-  if (sessionStorage.getItem('secondsLeft') && sessionStorage.getItem('minutesLeft')) {
-    // console.log('chto to est')
-    // console.log(sessionStorage.getItem('secondsLeft'))
-    seconds.innerHTML = sessionStorage.getItem('secondsLeft');
-    minutes.innerHTML = sessionStorage.getItem('minutesLeft');
+  pomodoro.initTimer();
+  if (sessionStorage.getItem('time')) {
+    console.log(sessionStorage.getItem('time'));
+    pomodoro.initTimeMs = sessionStorage.getItem('time');
+    pomodoro.updateDOM(pomodoro.initTimeMs);
   }
-}
-
-function initTimer() {
-  /* let initMinutes = parseInt(minutes.innerHTML);
-  let initSeconds = parseInt(seconds.innerHTML); */
-  
-  function calcRemTime() {
-    let secRem, minRem, timeRem;
-    if (sessionStorage.getItem('secondsLeft') && sessionStorage.getItem('minutesLeft')) {
-      secRem = sessionStorage.getItem('secondsLeft') * 1000;
-      minRem = sessionStorage.getItem('minutesLeft') * 60000;
-    } else {
-      secRem = initSeconds * 1000;
-      minRem = initMinutes * 60000;
-    }
-
-    return timeRem = secRem + minRem;
-  }
-
-  function convRemTime(remTime) {
-    let min = parseInt(remTime / 60000);
-    let sec = (remTime % 60000) / 1000;
-    let result = {
-      minutes: min,
-      seconds: sec
-    }
-
-    return result;
-  }
-
-  function stopTimer() {
-    clearInterval(countdown);
-    console.log('stop timer')
-  }
-
-  buttonStopMusic.addEventListener('click', stopTimer);
-
-  let remTimeInSec = calcRemTime();
-  console.log(remTimeInSec);
-
-  if (remTimeInSec === 0) {
-    return;
-  }
-
-  function startTimerCountdown() {
-    console.log('first action');
-
-    if (remTimeInSec > 0) {
-      remTimeInSec = remTimeInSec - 1000;
-
-      let remTime = convRemTime(remTimeInSec);
-
-      seconds.innerHTML = remTime.seconds;
-      minutes.innerHTML = remTime.minutes;
-
-      sessionStorage.setItem('minutesLeft', remTime.minutes);
-      console.log(sessionStorage.getItem('minutesLeft'));
-      sessionStorage.setItem('secondsLeft', remTime.seconds);
-      console.log(sessionStorage.getItem('secondsLeft'));
-
-      if (remTimeInSec === 0) {
-        sessionStorage.removeItem('minutesLeft');
-        sessionStorage.removeItem('secondsLeft');
-        clearInterval(countdown);
-      }
-    }
-    console.log('last action')
-  }
-
-  let countdown = setInterval(startTimerCountdown, 1000);
-}
-
-let minutes = document.querySelector('#countdown .minutes'); // меняющаяся цифра
-let seconds = document.querySelector('#countdown .seconds'); // меняющаяся цифра
-// let minutesLeft = parseInt(minutes.innerHTML);
-// let secondsLeft = parseInt(seconds.innerHTML);
-let initMinutes = parseInt(minutes.innerHTML);
-let initSeconds = parseInt(seconds.innerHTML);
-
-let buttonCountdown = document.querySelector('.btn-timer');
-// buttonCountdown.addEventListener('click', startCountdown);
-buttonCountdown.addEventListener('click', initTimer);
-
-let buttonStopMusic = document.querySelector('.stop-music');
-// buttonStopMusic.addEventListener('click', stopMusic)
-
-let buttonResetTimer = document.querySelector('.btn.reset');
-buttonResetTimer.addEventListener('click', () => {
-  seconds.innerHTML = initSeconds;
-  minutes.innerHTML = initMinutes;
-  if (sessionStorage.getItem('secondsLeft') && sessionStorage.getItem('minutesLeft')) {
-    sessionStorage.removeItem('secondsLeft');
-    sessionStorage.removeItem('minutesLeft');
-    console.log(sessionStorage.length);
-  }
-  if(countdown) {
-    clearInterval(countdown);
-  }
-});
+};
