c41e20b981491086ce931b941fc30ad9a0618874
Roman Tochilo <roman@roman.com>
2021-04-06 14:31:47 +0300

    js кастомное время

--- dist/main.js
+++ dist/main.js
@@ -4,16 +4,17 @@ let pomodoro = {
   initTimer() {
     this.secondsDom = document.querySelector('#countdown .seconds');
     this.minutesDom = document.querySelector('#countdown .minutes');
-    this.time = this.initTimeMs = this.convTimeToMs();
+    this.time = this.initTimeMs = this.convTimeToMs(this.secondsDom, this.minutesDom);
     console.log(this);
     document.querySelector('.btn-timer').addEventListener('click', this.startTimer);
     document.querySelector('.stop-music').addEventListener('click', this.pauseTimer);
     document.querySelector('.btn.reset').addEventListener('click', this.resetTimer);
+    document.querySelector('.btn.custom').addEventListener('click', this.applyCustomTime);
   },
-  convTimeToMs() {
+  convTimeToMs(seconds, minutes) {
     let secInMs, minInMs, timeInMs;
-    secInMs = this.secondsDom.innerHTML * 1000;
-    minInMs = this.minutesDom.innerHTML * 60000;
+    secInMs = seconds.innerHTML * 1000;
+    minInMs = minutes.innerHTML * 60000;
     return timeInMs = secInMs + minInMs;
   },
   convMsToNormTime(timeMs) {
@@ -65,8 +66,22 @@ let pomodoro = {
     console.log(time);
     let normTime = this.convMsToNormTime(time);
     console.log(normTime);
-    this.secondsDom.innerHTML = normTime.seconds;
-    this.minutesDom.innerHTML = normTime.minutes;
+    this.secondsDom.innerHTML = pomodoro.toDoubleDigit(normTime.seconds);
+    this.minutesDom.innerHTML = pomodoro.toDoubleDigit(normTime.minutes);
+  },
+  toDoubleDigit(num) {
+    if (num < 10) {
+      return "0" + parseInt(num, 10);
+    }
+    return num;
+  },
+  applyCustomTime() {
+    let minutes = document.querySelector('.custom-numbers .min').value * 60000;
+    let seconds = document.querySelector('.custom-numbers .sec').value * 1000;
+    console.log(minutes, seconds);
+    pomodoro.initTimeMs = minutes + seconds
+    console.log(pomodoro.initTimeMs);
+    pomodoro.updateDOM(pomodoro.initTimeMs);
   },
 
 };
